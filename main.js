039a0d85c2ed108ac6a7f60dab40e8950c7fa606
yashassvini-singh <152004611+yashassvini-singh@users.noreply.github.com>
2024-05-29 20:32:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(500,500);
+    canvas = createCanvas(400,400);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(400,400);
